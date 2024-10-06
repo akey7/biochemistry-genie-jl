723f6a5190e2778a37f3fc26e8c4159e8cb8c18d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 17:10:17 -0600

    Create Hill.jl

---
+++ lib/Hill.jl
@@ -0,0 +1,16 @@
+module Hill 
+
+export hill_eqn, ligand_concentrations
+
+function ligand_concentrations()
+    collect(range(start=0.0, stop=2.5e-3, length=100))
+end
+
+function hill_eqn(ka, n)
+    function hill(ligand_concentration)
+        ligand_concentration^n / (ka^n + ligand_concentration^n)
+    end
+    hill.(ligand_concentrations())
+end
+
+end
