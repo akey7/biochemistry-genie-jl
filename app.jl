637b99b32a78f5523e59c2308e4ecc34f1ac558b
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 12:18:00 -0600

    Update app.jl

--- app.jl
+++ app.jl
@@ -1,21 +1,19 @@
 module App
 
 using GenieFramework
-
 @genietools
 
 @app begin
-    @in name = "Genie"
+  @in foo = "Genie"
 end
 
 function ui()
-    [
-        h1("My first Genie app")
-        input("Enter your name", :name)
-        p("Hello {{message}}")
-    ]
+  [
+    h1("My First Genie App")
+    input("Enter your name", :foo)
+    p("Hello {{foo}}!")
+  ]
 end
 
 @page("/", ui)
-
 end
