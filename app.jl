02513f1bfb6da96f3bb030de442d1a81ea851dfc
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 17:04:34 -0600

    Update app.jl

--- app.jl
+++ app.jl
@@ -5,7 +5,9 @@ using GenieFramework, PlotlyBase
 
 @app begin
     @out trace1 = [histogram(x=[])]
+    @out trace2 = [PlotlyBase.scatter(x=[1,2,3], y=[1,2,3])]
     @out layout1 = PlotlyBase.Layout(title="Histogram plot")
+    @out layout2 = PlotlyBase.Layout(title="Line plot")
     @in N = 0
     @out m = 0.0
     @onchange N begin
@@ -21,7 +23,8 @@ function ui()
         h1("A simple dashboard"),
         slider(1:1000, :N),
         p("The average of {{N}} random numbers is {{m}}", class="st-module"),
-        plot(:trace1, layout=:layout1)
+        plot(:trace1, layout=:layout1),
+        plot(:trace2, layout=:layout2)
     ])
 ])
 end
