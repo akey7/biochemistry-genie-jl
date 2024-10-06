792bb4cc15ce6ace6e4bd63004ebb7b691b9bbfb
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 17:30:04 -0600

    Update app.jl

--- app.jl
+++ app.jl
@@ -5,17 +5,14 @@ using GenieFramework, PlotlyBase
 @genietools
 
 @app begin
-    @out trace1 = [histogram(x = [])]
     @out trace2 =
         [PlotlyBase.scatter(x = ligand_concentrations(), y = hill_eqn(1.0e-3, 1.0))]
-    @out layout1 = PlotlyBase.Layout(title = "Histogram plot")
     @out layout2 = PlotlyBase.Layout(title = "Line plot")
-    @in N = 0
-    @out m = 0.0
-    @onchange N begin
-        x = gen_numbers(N)
-        m = calc_mean(x)
-        trace1 = [histogram(x = x)]
+    @in hill_coeff_in = 1.0
+    @out hill_coeff_out = 1.0
+    @onchange hill_coeff_in begin
+        hill_coeff_out = round(hill_coeff_in, digits=2)
+        # trace1 = [histogram(x = x)]
     end
 end
 
@@ -24,13 +21,10 @@ function ui()
         cell(
             class = "st-col col-6",
             [
-                h4("A simple dashboard"),
-                slider(1:1000, :N),
-                p("The average of {{N}} random numbers is {{m}}", class = "st-module"),
-                plot(:trace1, layout = :layout1),
                 h4("Hill Equation"),
                 p("Select value for Hill coefficient (n):"),
-                slider(range(start=0.1, stop=4.0, length=20)),
+                slider(range(start=0.1, stop=4.0, length=20), :hill_coeff_in),
+                p("Hill coefficient: {{hill_coeff_out}}"),
                 plot(:trace2, layout = :layout2),
             ],
         ),
