4fa2305bdb8255963d30fb8a95bea47de310b7b5
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-06 17:26:48 -0600

    Update StatisticAnalysis.jl

--- lib/StatisticAnalysis.jl
+++ lib/StatisticAnalysis.jl
@@ -7,6 +7,6 @@ function gen_numbers(N::Int)
 end
 
 function calc_mean(x::Vector{Float64})
-    return round(sum(x) / length(x); digits=4)
+    return round(sum(x) / length(x); digits = 4)
 end
 end
