d63aa77f0026f5bc4ff9e75e4b473279d273ecb0
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-07 15:28:35 -0600

    Update app.jl

--- app.jl
+++ app.jl
@@ -3,33 +3,33 @@ using Main.Hill
 using GenieFramework, PlotlyBase
 @genietools
 
-@app begin
-    @out trace2 =
-        [PlotlyBase.scatter(x = ligand_concentrations(), y = hill_eqn(1.0e-3, 1.0))]
+function hill_eqn_trace(hill_coeff)
+    [PlotlyBase.scatter(x = ligand_concentrations(), y = hill_eqn(1.0e-3, hill_coeff))]
+end
 
-    @out layout2 = PlotlyBase.Layout(
-        title = "n = 1.0",
+function hill_eqn_layout(hill_coeff)
+    hill_coeff_formatted = round(hill_coeff, digits=2)
+
+    PlotlyBase.Layout(
+        title = "n = $hill_coeff_formatted",
         yaxis = attr(range = [0, 1], title = "Fraction of Sites Bound"),
         xaxis = attr(
             range = [minimum(ligand_concentrations()), maximum(ligand_concentrations())],
             title = "[L]",
         ),
     )
+end
 
+@app begin
+    @out trace2 = hill_eqn_trace(1.0)
+    @out layout2 = hill_eqn_layout(1.0)
     @in hill_coeff_in = 1.0
     @out hill_coeff_out = 1.0
 
     @onchange hill_coeff_in begin
         hill_coeff_out = round(hill_coeff_in, digits = 2)
-
-        trace2 = [
-            PlotlyBase.scatter(
-                x = ligand_concentrations(),
-                y = hill_eqn(1.0e-3, hill_coeff_in),
-            ),
-        ]
-        
-        layout2 = PlotlyBase.Layout(title = "n = $hill_coeff_out")
+        trace2 = hill_eqn_trace(hill_coeff_in)
+        layout2 = hill_eqn_layout(hill_coeff_in)
     end
 end
 
