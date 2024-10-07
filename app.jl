b42f0eb05a2e3b383467e5623e1fa38b5a8cadaa
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-07 15:13:54 -0600

    Update app.jl

--- app.jl
+++ app.jl
@@ -6,13 +6,19 @@ using GenieFramework, PlotlyBase
 @app begin
     @out trace2 =
         [PlotlyBase.scatter(x = ligand_concentrations(), y = hill_eqn(1.0e-3, 1.0))]
-    @out layout2 = PlotlyBase.Layout(title = "n = 1.0", yaxis = attr(range=[0, 1]))
+    @out layout2 = PlotlyBase.Layout(title = "n = 1.0", yaxis = attr(range = [0, 1]))
     @in hill_coeff_in = 1.0
     @out hill_coeff_out = 1.0
     @onchange hill_coeff_in begin
-        hill_coeff_out = round(hill_coeff_in, digits=2)
-        trace2 = [PlotlyBase.scatter(x = ligand_concentrations(), y = hill_eqn(1.0e-3, hill_coeff_in))]
-        layout2 = PlotlyBase.Layout(title = "n = $hill_coeff_out", yaxis = attr(range=[0, 1]))
+        hill_coeff_out = round(hill_coeff_in, digits = 2)
+        trace2 = [
+            PlotlyBase.scatter(
+                x = ligand_concentrations(),
+                y = hill_eqn(1.0e-3, hill_coeff_in),
+            ),
+        ]
+        layout2 =
+            PlotlyBase.Layout(title = "n = $hill_coeff_out", yaxis = attr(range = [0, 1]))
     end
 end
 
@@ -23,7 +29,7 @@ function ui()
             [
                 h4("Hill Equation"),
                 p("Select value for Hill coefficient (n):"),
-                slider(range(start=0.1, stop=4.0, length=20), :hill_coeff_in),
+                slider(range(start = 0.1, stop = 4.0, length = 20), :hill_coeff_in),
                 p("Hill coefficient: {{hill_coeff_out}}"),
                 plot(:trace2, layout = :layout2),
             ],
