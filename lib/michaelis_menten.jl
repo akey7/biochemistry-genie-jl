280c351323c2e621f0c45db0fd5820bbcdbeef7d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-07 22:58:39 -0600

    Update michaelis_menten.jl

--- lib/michaelis_menten.jl
+++ lib/michaelis_menten.jl
@@ -1,6 +1,17 @@
 module MichaelisMenten
 
-export substrate_concentrations, km_range, vmax_range, mm_eqn
+export substrate_concentrations, km_range, vmax_range, mm_eqn, set_km_cache, set_vmax_cache, km_cache, vmax_cache
+
+km_cache = 0.0
+vmax_cache = 0.0
+
+function set_vmax_cache(vmax)
+    global vmax_cache = vmax
+end
+
+function set_km_cache(km)
+    global km_cache = km
+end
 
 function substrate_concentrations()
     collect(range(start=0, stop=0.1, length=100))
