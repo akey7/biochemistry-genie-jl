8cd7ba83ef16269a44f23fae3a61192d58873a9c
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-07 19:54:32 -0600

    Update mm.jl

--- mm.jl
+++ mm.jl
@@ -1,4 +1,4 @@
-module HillEqnPage
+module MichaelisMentenPage
 using Printf
 using Main.MichaelisMenten
 using GenieFramework
@@ -6,13 +6,20 @@ using PlotlyBase
 @genietools
 
 @app begin
-    
+    vmax_midpoint = (minimum(vmax_range()) + maximum(vmax_range())) / 2.0
+    km_midpoint = (minimum(km_range()) + maximum(km_range())) / 2.0
+    @in vmax_in = vmax_midpoint
+    @in km_in = km_midpoint
 end
 
 function mm_ui()
     [
         a("Return to demonstration list", href = "/"),
-        h4("Michaelis-Menten")
+        h4("Michaelis-Menten"),
+        p("Select a Vmax:"),
+        slider(vmax_range(), :vmax_in, labelalways=true),
+        p("Select a Km:"),
+        slider(km_range(), :km_in, labelalways=true),
     ]
 end
 
