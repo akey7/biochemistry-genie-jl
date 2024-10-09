acbd770e7020f05bb63ffd05f058ff062688f8f3
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-10-09 15:42:26 -0600

    Update linear_pathway_page.jl

--- linear_pathway_page.jl
+++ linear_pathway_page.jl
@@ -2,10 +2,16 @@ module LinearPathwayPage
 using GenieFramework
 @genietools
 
+@app begin
+    @in Range_r = RangeData(1:5)
+end
+
 function ui()
     [
         a("Return to demonstration list", href = "/"),
-        h4("Linear pathway!")
+        h4("Linear pathway!"),
+        p("Select Input 2 availability:"),
+        GenieFramework.range(1:1:30, :Range_r, label = true)
     ]
     
 end
